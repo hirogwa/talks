29c810749c44b28fca38e7f56060fee4a6f872d9
hirogwa <hirogwa@gmail.com>
2014-12-08 22:00:22 +0000

    add example

---
+++ simball/examples/hello.py
@@ -0,0 +1,19 @@
+"""
+My friendly greeting module.
+"""
+class MyAwesomeClass:
+    """
+    Do stuff.
+    """
+
+    @staticmethod
+    def say_hello():
+        print ('hello world')
+        print ('hello world')
+    @staticmethod
+    def say_hello_again():
+
+
+if __name__ = '__main__':
+    MyAwesomeClass.say_hello()
+
